76ef49440fcc4f4aded297befea5b3111b807220
Juhi <65873340+Juhiiiii@users.noreply.github.com>
2026-02-10 21:27:08 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCfIgBhw-9WHcVX8f8AfFt_ldNZmL9C0mo",
+    authDomain: "ashjuhi.firebaseapp.com",
+    projectId: "ashjuhi",
+    storageBucket: "ashjuhi.firebasestorage.app",
+    messagingSenderId: "491701119041",
+    appId: "1:491701119041:web:afc3ea24dacc36495c7ec0"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
